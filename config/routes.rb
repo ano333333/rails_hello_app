f1f09afe3bafb5394850bb43f4914bf727b5a31b
ano3 <172483903+ano333333@users.noreply.github.com>
2025-05-05 15:31:27 +0000

    feat: 12章

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'users/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -16,4 +18,5 @@ Rails.application.routes.draw do
   resources :microposts, only: %i[create destroy]
   resources :account_activations, only: [:edit]
   resources :relationships, only: %i[create destroy]
+  resources :password_resets, only: %i[new create edit update]
 end
